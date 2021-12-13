06b5228c766937b6f399012eb3f156f3fe38ea16
Ishikawa Koji <ijuok.i@gmail.com>
2021-12-13 14:21:12 +0900

    read csv
    read CSV from upleaded file

--- streamlit_scatter3d.py
+++ streamlit_scatter3d.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.title('scatter 3D for car coating')
+
+upfile = st.file_uploader('select file')
+if upfile is not None:
+  df = pd.read_csv(upfile)
+  st.write(df)
 
