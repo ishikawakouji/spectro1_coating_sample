37ca281fcb26da1ec31ac5ec1b1aa1f7da4e3ebe
Ishikawa Koji <ijuok.i@gmail.com>
2021-12-13 15:10:39 +0900

    show if file has uploaded

--- streamlit_scatter3d.py
+++ streamlit_scatter3d.py
@@ -8,6 +8,5 @@ upfile = st.file_uploader('select file')
 if upfile is not None:
   df = pd.read_csv(upfile)
   st.write(df)
-
-fig = px.scatter_3d(df, x='A-2deg-L', y='A-2deg-a', z='A-2deg-b', color='part')
-fig.show()
+  fig = px.scatter_3d(df, x='A-2deg-L', y='A-2deg-a', z='A-2deg-b', color='part')
+  fig.show()
