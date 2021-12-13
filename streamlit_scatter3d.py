efc3dcd8773da6b3eea9c664cd168ea0bd7738b4
Ishikawa Koji <ijuok.i@gmail.com>
2021-12-13 14:24:56 +0900

    add plotly writing scatter 3d

--- streamlit_scatter3d.py
+++ streamlit_scatter3d.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 st.title('scatter 3D for car coating')
 
@@ -8,3 +9,5 @@ if upfile is not None:
   df = pd.read_csv(upfile)
   st.write(df)
 
+fig = px.scatter_3d(df, x='A-2deg-L', y='A-2deg-a', z='A-2deg-b', color='part')
+fig.show()
