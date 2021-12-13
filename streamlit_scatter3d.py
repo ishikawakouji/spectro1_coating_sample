652bf82b6cad6724ec4cdbd24baab10182af73a3
Ishikawa Koji <ijuok.i@gmail.com>
2021-12-13 15:40:43 +0900

    selectbox

--- streamlit_scatter3d.py
+++ streamlit_scatter3d.py
@@ -5,6 +5,7 @@ import plotly.express as px
 st.title('scatter 3D for car coating')
 
 labelhead = 'A-2deg'
+
 xl = labelhead + '-L'
 yl = labelhead + '-a'
 zl = labelhead + '-b'
@@ -13,10 +14,15 @@ def plot_plotly(df):
   fig = px.scatter_3d(df, x=xl, y=yl, z=zl, color='part')
   st.plotly_chart(fig, use_container_width=True)
 
-st.sidebar.write('control')
 
 upfile = st.sidebar.file_uploader('select file')
 
 if upfile is not None:
   df = pd.read_csv(upfile)
   plot_plotly(df)
+  
+labelhead = st.sidebar.selectbox('select', ('A-2deg', 'A-10deg'))
+xl = labelhead + '-L'
+yl = labelhead + '-a'
+zl = labelhead + '-b'
+plot_plotly(df)
